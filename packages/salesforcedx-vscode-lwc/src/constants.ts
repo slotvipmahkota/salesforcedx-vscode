b13baaf1b1196e3678fc00f65368acdc49805380
Daphne Yang <daphne.yang@salesforce.com>
2023-08-25 15:49:15 -0400

    chore: change double quotes to single quotes to pass lint

--- packages/salesforcedx-vscode-lwc/src/constants.ts
+++ packages/salesforcedx-vscode-lwc/src/constants.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { channelService } from "./channel";
+import { channelService } from './channel';
 
 export const ESLINT_NODEPATH_CONFIG = 'eslint.nodePath';
 export const LWC_EXTENSION_NAME = 'salesforcedx-vscode-lwc';
