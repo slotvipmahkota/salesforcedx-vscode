8f83ca1e81749f14971eadce06d3e4247e5eb83c
mingxuanzhang <mingxuanzhang@salesforce.com>
2024-05-09 14:47:50 -0700

    chore: bundle core-bundle & stl

--- packages/salesforcedx-vscode-core/esbuild.config.js
+++ packages/salesforcedx-vscode-core/esbuild.config.js
@@ -6,7 +6,8 @@
  */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { build } = require('esbuild');
-// const fs = require('fs').promises;
+const esbuildPluginPino = require('esbuild-plugin-pino');
+const fs = require('fs').promises;
 
 const sharedConfig = {
   bundle: true,
@@ -16,36 +17,62 @@ const sharedConfig = {
     'vscode',
     'applicationinsights',
     'shelljs',
-    '@salesforce/core-bundle',
+    'node-gyp/bin/node-gyp.js'
+    // '@salesforce/core-bundle',
     // '@salesforce/source-tracking-bundle',
-    '@salesforce/templates-bundle'
+    // '@salesforce/templates-bundle'
     // '@salesforce/source-deploy-retrieve-bundle'
   ],
-  minify: true
+  minify: true,
+  keepNames: true,
+  plugins: [
+    esbuildPluginPino({ transports: ['pino-pretty'] })
+  ]
 };
 
 // copy core-bundle/lib/transformStream.js to dist if core-bundle is included
-// const copyFiles = async (src, dest) => {
-//   try {
-//     // Copy the file
-//     await fs.copyFile(src, dest);
-//     console.log(`File was copied from ${src} to ${dest}`);
-//   } catch (error) {
-//     console.error('An error occurred:', error);
-//   }
-// };
+const copyFiles = async (src, dest) => {
+  try {
+    // Extract the directory path from the destination path
+    const dir = require('path').dirname(dest);
 
-// const srcPath = '../../node_modules/@salesforce/core-bundle/lib/transformStream.js';
-// const destPath = './dist/transformStream.js';
+    // Ensure the destination directory exists
+    await fs.mkdir(dir, { recursive: true });
+
+    // Copy the file
+    await fs.copyFile(src, dest);
+    console.log(`File was copied from ${src} to ${dest}`);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+};
+
+const srcPath = '../../node_modules/@salesforce/core-bundle/lib/transformStream.js';
+const destPath = './dist/transformStream.js';
+
+const nodeGypSrc = '../../node_modules/yeoman-environment/node_modules/node-gyp/bin/node-gyp.js';
+const nodeGypDest = './dist/node-gyp.js';
+
+const resolveNodeGypRef = async () => {
+  const entryPath = 'dist/index.js';
+  let bundledEntryPoint = await fs.readFile(entryPath, 'utf8');
+  const searchString = /node-gyp\/bin\/node-gyp\.js/g;
+  const replacementString = './node-gyp.js';
+  bundledEntryPoint = bundledEntryPoint.replace(searchString, replacementString);
+  await fs.writeFile(entryPath, bundledEntryPoint, 'utf8');
+  console.log('Redirect reference of node-gyp to dist successfully');
+};
 
 (async () => {
   await build({
     ...sharedConfig,
     entryPoints: ['./src/index.ts'],
-    outfile: 'dist/index.js'
+    outdir: 'dist'
   });
 })()
   .then(async () => {
-    // await copyFiles(srcPath, destPath);
+    await copyFiles(srcPath, destPath);
+    await copyFiles(nodeGypSrc, nodeGypDest);
+    await resolveNodeGypRef();
   })
   .catch(() => process.exit(1));
